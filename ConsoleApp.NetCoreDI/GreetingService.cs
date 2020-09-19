45934c5decc22ad2599782e5b2efc3bd86ded590
Gowri Krishnamurthy <kr_gowri@yahoo.com>
2020-09-19 16:17:42 +1000

    DI, Serilog, settings

---
+++ ConsoleApp.NetCoreDI/GreetingService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp.NetCoreDI
+{
+    public interface IGreetingService
+    {
+        void Run();
+    }
+
+    public class GreetingService : IGreetingService
+    {
+        private readonly ILogger<GreetingService> _log;
+        private readonly IConfiguration _config;
+
+        public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+        public void Run()
+        {
+            for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+            {
+                _log.LogInformation("Run number {runNumber}", i);
+
+            }
+        }
+    }
+}
--- ConsoleApp.NetCoreDI/Program.cs
+++ ConsoleApp.NetCoreDI/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace ConsoleApp.NetCoreDI
 {
+    // DI, Serilog, settings
+
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom
+                .Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            Log.Logger.Information("Application Settings");
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IGreetingService, GreetingService>();
+                })
+                .UseSerilog()
+                .Build();
+
+            var service = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+            service.Run();
+
+        }
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json",
+                    optional: false,
+                    reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                    optional: true,
+                    reloadOnChange: true)
+                .AddEnvironmentVariables();
         }
     }
 }
